ebec87768e3667060a9e467683c89fb54c1f5c21
Utkarsh Gupta <Utkarsh.Gupta@setindia.com>
2025-04-24 21:23:30 +0530

    Task list

--- reactapp/src/components/taskList/index.jsx
+++ reactapp/src/components/taskList/index.jsx
@@ -1,20 +1,40 @@
+import { useState } from "react";
 import TaskItem from "./taskItem";
 import styles from "./tasklist.module.css";
 const TaskList = (props) => {
   const { title } = props;
 
-  const tasks = [
+  let [input, setInput] = useState("");
+  const [tasks, setTasks] = useState([
     { task: "Wake Up", id: "12121" },
     { task: "Break Fast", id: "3232" },
     { task: "Work", id: "4343" },
     { task: "Rest", id: "43121" },
     { task: "Play", id: "3238" },
     { task: "Sleep", id: "9893" },
-  ];
+  ]);
+
   const total = tasks.length;
+
+  const onInput = (e) => {
+    const { value } = e.target;
+    setInput(value);
+  };
+
+  const onAdd = () => {
+    let id = parseInt(Math.random() * 1000); // a random 3 digit id
+    setTasks([{ id, task: input }, ...tasks]);
+    /* creating a new state array, 
+      copying older tasks and adding new task with id*/
+    setInput("");
+  };
   return (
     <div className={styles.list_container}>
       <h2 className={styles.list_title}>{title}</h2>
+
+      <input value={input} placeholder="Enter a task...." onChange={onInput} />
+      <button onClick={onAdd}>Add Task</button>
+
       <h3 className={styles.list_total}>Total Tasks {total}</h3>
 
       {total > 0 && (
